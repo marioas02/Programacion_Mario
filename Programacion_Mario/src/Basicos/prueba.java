22ded33651a08447c70e0e853192c0f861987f6d
MarioAguirre <MarioAguirre@DESKTOP-KF8T2TO.localdomain>
2020-11-25 08:38:11 +0100

    26-11-2020 - EEP

---
+++ Programacion_Mario/src/Basicos/prueba.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Basicos;
+
+/**
+ * @author MarioAguirre
+ *
+ */
+public class prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
