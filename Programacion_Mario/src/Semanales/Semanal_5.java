67665bc8ff3f742488c50f647535c37aa92a5526
magui <magui@DESKTOP-RCDC17H.home>
2020-11-25 20:10:09 +0100

    25-11-2020 - CASA

--- Programacion_Mario/src/Semanales/Semanal_5.java
+++ Programacion_Mario/src/Semanales/Semanal_5.java
@@ -26,9 +26,8 @@ public class Semanal_5 {
 		
 		do {
 			
-		
 //LE EXPLICO LAS PROPIEDADES DEL PROGRAMA.
-		System.out.printf("Los palos son: \n\t d = DIAMANTES \n\t p = PICAS \n\t t = TREBOLES \n\t c = CORAZONES.\n");
+			System.out.printf("Los palos son: \n\t d = DIAMANTES \n\t p = PICAS \n\t t = TREBOLES \n\t c = CORAZONES.\n");
 	
 //PIDO DATOS Y MIRO HABER SI CUMPLE LAS PROPIEDADES.
 			for(int i=0;i<numero.length;i++) {
@@ -51,6 +50,7 @@ public class Semanal_5 {
 					i=i-1;
 				}
 			}
+			
 //ORDENO LOS NUMEROS PARA DEPUES MIRAR DE QUE TIPO DE ESCALERA ES.
 			Arrays.sort(numero);
 			
@@ -61,63 +61,76 @@ public class Semanal_5 {
 			else {
 			
 //MIRO Y FILTRO QUE TIPO ES PARA DECIRLE QUE CARTA LE FALTA.		
-				if(palos[0]!=palos[1]&&palos[0]!=palos[2]&&palos[0]!=palos[3]) {
-					System.out.println("No se puede hacer escalera.");
-				}
-				
+					if(palos[0]!=palos[1]&&palos[0]!=palos[2]&&palos[0]!=palos[3]) {
+						System.out.println("No se puede hacer escalera.");
+					}
+					
 //ORDENADO CON EL 0=AS		
-				else if(numero[0]==1&&numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1&&numero[3]==numero[2]+1) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[3]+1, palos[0]);
-				}
-				
+					else if(numero[0]==1&&numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1&&numero[3]==numero[2]+1) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[3]+1, palos[0]);
+					}
+					
 //ORDEN CON EL 0=AS Y 1=11 Y 2=12 Y 3=13
-				else if(numero[0]==1&&numero[3]==13&&numero[1]==11&&numero[2]==12) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[1]-1, palos[0]);
-				}
-				
+					else if(numero[0]==1&&numero[3]==13&&numero[1]==11&&numero[2]==12) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[1]-1, palos[0]);
+					}
+					
 //ORDENADO CON EL 3=13		
-				else if(numero[3]==13&&numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1&&numero[3]==numero[2]+1) {
-					System.out.printf("Para hacer escalera te falta: %d %c", 1, palos[0]);
-				}
-				
+					else if(numero[3]==13&&numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1&&numero[3]==numero[2]+1) {
+						System.out.printf("Para hacer escalera te falta: %d %c", 1, palos[0]);
+					}
+					
 //ORDENADO TODA LAS CARTAS SIN EL 1 NI EL 13		
-				else if(numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1&&numero[3]==numero[2]+1) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[3]+1, palos[0]);
-				}
-				
+					else if(numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1&&numero[3]==numero[2]+1) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[3]+1, palos[0]);
+					}
+					
 //DESORDEN EL ENTRE EL 0 Y EL 1		
-				else if(numero[0]==numero[1]-2&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[1]-1,palos[0]);
-				}
-				
+					else if(numero[0]==numero[1]-2&&numero[1]==numero[2]-1&&numero[2]==numero[3]-1) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[1]-1,palos[0]);
+					}
+					
 //DESORDEN EL ENTRE EL 1 Y EL 2		
-				else if(numero[0]==numero[1]-1&&numero[1]==numero[2]-2&&numero[2]==numero[3]-1) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[2]-1,palos[0]);
-				}
-				
-//DESORDEN EL ENTRE EL 2 Y EL 3		
-				else if(numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-2) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[3]-1,palos[0]);
-				}
-				
-//DESORDEN EN LA ROTATIVA 1,13,12,11,10
-				else if(numero[0]==1&&numero[3]==12&&numero[1]==10&&numero[2]==11) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[3]+1,palos[0]);
-				}
-				else if(numero[0]==1&&numero[3]==13&&numero[1]==10&&numero[2]==12) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[1]+1,palos[0]);
-				}
-				else if(numero[0]==1&&numero[3]==13&&numero[1]==10&&numero[2]==11) {
-					System.out.printf("Para hacer escalera te falta: %d %c", numero[1]+2,palos[0]);
-				}
-				else {
-					System.out.println("No se puede hacer escalera.");
-				}
+					else if(numero[0]==numero[1]-1&&numero[1]==numero[2]-2&&numero[2]==numero[3]-1) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[2]-1,palos[0]);
+					}
+					
+	//DESORDEN EL ENTRE EL 2 Y EL 3		
+					else if(numero[0]==numero[1]-1&&numero[1]==numero[2]-1&&numero[2]==numero[3]-2) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[3]-1,palos[0]);
+					}
+					
+	//DESORDEN EN LA ROTATIVA 1,13,12,11,10
+					else if(numero[0]==1&&numero[3]==12&&numero[1]==10&&numero[2]==11) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[3]+1,palos[0]);
+					}
+					else if(numero[0]==1&&numero[3]==13&&numero[1]==10&&numero[2]==12) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[1]+1,palos[0]);
+					}
+					else if(numero[0]==1&&numero[3]==13&&numero[1]==10&&numero[2]==11) {
+						System.out.printf("Para hacer escalera te falta: %d %c", numero[1]+2,palos[0]);
+					}
+					else {
+						System.out.println("No se puede hacer escalera.");
+					}
 			}
+			
 			System.out.printf("\n\n\tSi quiere salir pulse 2, si quiere otra mano pulse 1: \n\n");
 			salir=entrada.nextInt();
 			cont=1;
+			
 			}while(salir==1);
 		entrada.close();
 	}
 }
+
+//PSEUDOCÓDIGO:
+//	
+//	DECLARAR LAS 2 ARRAY DE PALOS Y LETRAS, UN CONTADOR INICIALIZARLO A 1 Y EL SALI A 1.
+//	CREAR UN BUCLE CON LA CONDICION DE SALIDA.
+//	CREAR UN BUCLE PARA PEDIR DATOS Y METERLOS AL ARRAY DE NUMEROS Y PALOS Y PONERLOS EN EL ARRAY CORRESPONDIENTE.
+//	COMPROBAR QUE LOS NUMEROS NO SEAN MAYORES QUE 13 O MENORES QUE 1.
+//	COMPROBAR QUE LOS PALOS SON IGUALES.
+//	COMPROBAR TODAS LAS ESCALERAS Y MIRAR SI CONCUERDA CON ALGUN FILTRO.
+//	SI NO DECIR QUE NO HAY ESCALERA.
+//	PREGUNTAR SI QUIERE FINALIZAR CON LA CONIDCION DEL BUCLE. 
