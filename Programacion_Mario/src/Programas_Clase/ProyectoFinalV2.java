4e306a5841c1d2bb2ba17fe3ad4c245456a278d6
magui <magui@DESKTOP-RCDC17H.home>
2020-11-27 00:03:32 +0100

    27-11-2020 - CASA

--- Programacion_Mario/src/Programas_Clase/ProyectoFinalV2.java
+++ Programacion_Mario/src/Programas_Clase/ProyectoFinalV2.java
@@ -21,18 +21,18 @@ public class ProyectoFinalV2 {
 		// CLICAR ALGO,
 		// PREGUNTA PARA NAIRA: SON PREDETERMINADAS LAS FILAS Y COLUMNAS?
 
-		int numero[][] = new int[10][10];
-		String mapa[][] = new String[10][10];
-		int fh = 0, ch = 0,paso=0;
+		int numero[][] = new int[11][11];
+		String mapa[][] = new String[11][11];
+		int fh = 0, ch = 0,paso=1;
 		char direccion;
 		
 
 //DONDE QUIERE EL USUARIO EL HEROE
-		System.out.printf("Diga la fila que quiere poner al heroe: ");
-		fh = entrada.nextInt();
-		System.out.print("\n");
-		System.out.printf("Diga la columna que quiere poner al heroe: ");
-		ch = entrada.nextInt();
+//		System.out.printf("Diga la fila que quiere poner al heroe: ");
+//		fh = entrada.nextInt();
+//		System.out.print("\n");
+//		System.out.printf("Diga la columna que quiere poner al heroe: ");
+//		ch = entrada.nextInt();
 		
 //RECORRER EL ARRAY METIENDO NUMEROS ALEATORIOS DEL 1 AL 2
 		for (int i = 0; i < numero.length; i++) {
@@ -50,9 +50,24 @@ public class ProyectoFinalV2 {
 				}
 			}
 		}
+		for (int i = 0; i < numero.length; i++) {
+			System.out.print("\n");
+			for (int j = 0; j < numero[i].length; j++) {
+				System.out.print(numero[i][j] + " ");
+			}
+		}
+		System.out.printf("Diga la fila que quiere poner al heroe: ");
+		fh = entrada.nextInt();
+		System.out.print("\n");
+		System.out.printf("Diga la columna que quiere poner al heroe: ");
+		ch = entrada.nextInt();
 		for (int i = 0; i < 1; i++) {
 			mapa[fh][ch] = "H";
 		}
+		for (int i = 0; i < 1; i++) {
+			numero[fh][ch] = 3;
+		}
+		
 //PINTAR EL MAPA
 		
 		for (int i = 0; i < mapa.length; i++) {
@@ -70,20 +85,27 @@ public class ProyectoFinalV2 {
 		
 //MIRASIONES DE DIRESIONES
 		int aux=0;
+		
 		aux=ch;
 		if(direccion=='r') {
-			while(numero[fh][aux]<2){
-				
-				if(numero[fh][ch+1]==1) {
-					ch++;
-					paso++;
+			if(ch!=10) {
+				while(numero[fh][aux]>2&&aux<10){
 					
-				}else {
-					System.out.printf("Ha dado: %d pasos", paso);
+					if(numero[fh][ch+1]==2&&ch<10) {
+						ch++;
+						paso++;
+					}else if(numero[fh][ch+1]==1 && ch<10||ch==10) {
+						System.out.printf("Ha dado: %d pasos", paso);
+					}
+					aux++;
 				}
-				aux++;
+				
+			}else {
+				System.out.printf("Estas en el borde del mapa, no tiene villanos a la derecha");
 			}
+			
 		}
+	
 		
 		
 		
