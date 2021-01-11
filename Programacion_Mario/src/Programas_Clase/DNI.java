106e1318c840eefb2ef961797545e3910984eacf
magui <magui@DESKTOP-RCDC17H>
2021-01-11 12:46:09 +0100

    11-01-20201 - CASA

--- Programacion_Mario/src/Programas_Clase/DNI.java
+++ Programacion_Mario/src/Programas_Clase/DNI.java
@@ -15,34 +15,41 @@ public class DNI {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in); 
-
+        Scanner sc=new Scanner(System.in);
         
-
-        int dni; 
-
-        char letra[]= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'}; 
-
-        int resto; 
-
-         
-
+        int dni;
         System.out.println("Escribe tu numero del DNI y te digo la letra que tiene"); 
-
-        dni=sc.nextInt(); 
-
-         
-
-        resto=dni %23; 
-
-         
-
-        System.out.println("La letra de tu DNI es "+letra[resto]); 
-
-         
+        dni=sc.nextInt();
+		System.out.println("La letra de tu DNI es "+ letraDNI(dni)); 
 
         sc.close(); 
-
 	}
-
+	
+	private static char letraDNI(int dni) {
+		char resultado;
+        char letra[]= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'}; 
+        int resto;
+        resto=dni %23;
+        resultado =letra[resto];
+		return resultado;
+	}
+	
 }
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- Programacion_Mario/src/Programas_Clase/_71_EjericicioTeams.java
+++ Programacion_Mario/src/Programas_Clase/_71_EjericicioTeams.java
@@ -14,53 +14,67 @@ public class _71_EjericicioTeams {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [][] a = new int[3][3]; 
-        int [][] b = new int[3][3]; 
-        int [][] c = new int[3][3];
-        //Cargar tabla a con numeros aleatorios             
-            for (int i=0; i<a.length; i++) {                 
-                for (int j=0; j<a[i].length; j++) {                   
-                    a[i][j]=(int)(Math.random()*10); 
-                } 
-            }
-            //Cargar tabla b con numeros aleatorios          
-            for (int i=0; i<b.length; i++) { 
-                for (int j=0; j<b[i].length; j++) {                       
-                    b[i][j]=(int)(Math.random()*10); 
-                } 
-            }
-            //Hacer suma (que se mete en tabla c) 
-            for (int i=0; i<c.length; i++) { 
-                for (int j=0; j<c[i].length; j++) { 
-                    c[i][j] =  a[i][j] + b[i][j];                   
-                } 
-            }
-            //pintar tabla a  
-            System.out.println("\nEste es el primer array"); 
-            for (int i=0; i<a.length; i++) { 
-                for (int j=0; j<a[i].length; j++) { 
-                    System.out.print("P." +i+ "." +j+ "| "); 
-                    System.out.print(a[i][j]+ " \t");                     
-                } 
-                System.out.print("\n"); 
-            }
-            //pintar tabla b 
-            System.out.println("\nEste es el segundo array"); 
-            for (int i=0; i<b.length; i++) { 
-                for (int j=0; j<b[i].length; j++) { 
-                    System.out.print("P." +i+ "." +j+ "| "); 
-                    System.out.print(b[i][j]+ " \t"); 
-                } 
-                System.out.print("\n"); 
-            }
-            //pintar tabla c (tercer array, la suma) 
-            System.out.println("\nEste es el tercer array (la suma de los dos primeros arrays)");
-            for (int i=0; i<c.length; i++) { 
-                for (int j=0; j<c[i].length; j++) { 
-                    System.out.print("P." +i+ "." +j+ "| " +a[i][j]+ " + " +b[i][j]+ " = ");  
-                    System.out.print(c[i][j]+ " \t"); 
-                }
-                System.out.print("\n");
-            }
+		int[][] a = new int[3][3];
+		int[][] b = new int[3][3];
+		int[][] c = new int[3][3];
+		// Cargar tabla a con numeros aleatorios
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				a[i][j] = (int) (Math.random() * 10);
+			}
+		}
+		
+		int numero[][]=new int [3][3];
+		fillrandom(numero);
+		
+		// Cargar tabla b con numeros aleatorios
+		for (int i = 0; i < b.length; i++) {
+			for (int j = 0; j < b[i].length; j++) {
+				b[i][j] = (int) (Math.random() * 10);
+			}
+		}
+		// Hacer suma (que se mete en tabla c)
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				c[i][j] = a[i][j] + b[i][j];
+			}
+		}
+		// pintar tabla a
+		System.out.println("\nEste es el primer array");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print("P." + i + "." + j + "| ");
+				System.out.print(a[i][j] + " \t");
+			}
+			System.out.print("\n");
+		}
+		// pintar tabla b
+		System.out.println("\nEste es el segundo array");
+		for (int i = 0; i < b.length; i++) {
+			for (int j = 0; j < b[i].length; j++) {
+				System.out.print("P." + i + "." + j + "| ");
+				System.out.print(b[i][j] + " \t");
+			}
+			System.out.print("\n");
+		}
+		// pintar tabla c (tercer array, la suma)
+		System.out.println("\nEste es el tercer array (la suma de los dos primeros arrays)");
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				System.out.print("P." + i + "." + j + "| " + a[i][j] + " + " + b[i][j] + " = ");
+				System.out.print(c[i][j] + " \t");
+			}
+			System.out.print("\n");
+		}
 	}
+
+	private static void fillrandom(int[][] numero) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				a[i][j] = (int) (Math.random() * 10);
+			}
+		}
+	}
+
+	
 }
