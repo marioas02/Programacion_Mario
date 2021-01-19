1ea7693f3bc78356e9a44e0034fe3a220ca1c090
magui <magui@DESKTOP-RCDC17H>
2021-01-19 13:28:30 +0100

    19/01/2021 - CASA

--- Programacion_Mario/src/UT6_20/Persona.java
+++ Programacion_Mario/src/UT6_20/Persona.java
@@ -34,16 +34,65 @@ public class Persona {
 	public String getNombre() {
 		return this.nombre;
 	}
-	public int getAnio() {
+	public String getApillidos() {
+		return this.apellidos;
+	}
+	public int getAnioNacimiento() {
 		return this.anioNacimiento;
 	}
-	// TODO crear resto de getters
+	public int getMesNacimiento() {
+		return this.mesNacimiento;
+	}
+	public int getDiaNacimiento() {
+		return this.diaNacimiento;
+	}
+	public char getSexo() {
+		return this .sexo;
+	}
 
 	//Setters
 	public void setNombre(String nombre) {
+		//Convierto el nombre a palabra con la primera en mayusculas
+		nombre = toUpperPrimeraLetra(nombre);
 		if(nombre.length()<4)
 			nombre="Sin especificar";
+		
 		this.nombre = nombre;
 	}
-	// TODO crear resto de setters
+	public void setApellidos(String apellidos) {
+		//Convierto el nombre a palabra con la primera en mayusculas
+		nombre = toUpperPrimeraLetra(nombre);
+		if(nombre.length()<4)
+			nombre="Sin especificar";
+		
+		this.nombre = nombre;
+	}
+	
+	public void setAnio(int anio) {
+		if(anio<1900 || anio>2020)
+			anio = 2000;
+		
+		this.anioNacimiento = anio;
+	}
+	
+	public void setMes(int mes) {
+		if(mes<1 || mes>12)
+			mes = 1;
+		
+		this.mesNacimiento = mes;
+	}
+	
+	public void setDia(int dia) {
+		if(dia<1 || dia>31)
+			dia = 1;
+		
+		this.diaNacimiento = dia;
+	}
+	
+	private String toUpperPrimeraLetra(String palabra) {
+		String npropio = palabra;
+		//TODO implementar funcion
+		
+		return npropio;
+	}
 }
