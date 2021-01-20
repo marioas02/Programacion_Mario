aaa0800f35df2c9748dc2b2407236fc95ec824d4
magui <magui@DESKTOP-RCDC17H>
2021-01-20 16:43:57 +0100

    20/01/2021 - CASA

--- Programacion_Mario/src/UT6_20/Actividad20.java
+++ Programacion_Mario/src/UT6_20/Actividad20.java
@@ -15,10 +15,21 @@ public class Actividad20 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Persona yo = new Persona();
-		
-		yo.setNombre("Mario");
+		//Prueba
+		Persona yo = new Persona();		
+		yo.setNombre("mario");
 		System.out.println(yo.getNombre());
+		yo.setApellidos("Aguirre sanz");
+		System.out.println(yo.getApellidos());
+		yo.setAnio(2002);
+		yo.setMes(01);
+		yo.setDia(15);
+		yo.setSexo('H');
+		System.out.println(yo.saludar());
+		System.out.println(yo.mostrarEdad());
+		
+		Persona invento = new Persona("PEPE", "LOPEZ garcia", 8000, 34, -3, 'G');
+		System.out.println(invento.saludar());
 	}
 
 }
--- Programacion_Mario/src/UT6_20/Persona.java
+++ Programacion_Mario/src/UT6_20/Persona.java
@@ -22,19 +22,19 @@ public class Persona {
 	
 	//Constructor
 	public Persona(String nombre, String apellidos, int anio, int mes, int dia, char sexo) {
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.anioNacimiento = anio;
-		this.mesNacimiento = mes;
-		this.diaNacimiento = dia;
-		this.sexo = sexo;
+		setNombre(nombre);
+		setApellidos(apellidos);
+		setAnio(anio);
+		setMes(mes);
+		setDia(dia);
+		setSexo(sexo);
 	}
 	
-	//Getter
+	//Getter públicos para todos los atributos
 	public String getNombre() {
 		return this.nombre;
 	}
-	public String getApillidos() {
+	public String getApellidos() {
 		return this.apellidos;
 	}
 	public int getAnioNacimiento() {
@@ -49,50 +49,79 @@ public class Persona {
 	public char getSexo() {
 		return this .sexo;
 	}
+	public int getEdad() {
+		int edad = java.time.LocalDate.now().getYear() - this.anioNacimiento;
+		return edad;
+	}
 
-	//Setters
+	//Setters públicos para todos los atributos
 	public void setNombre(String nombre) {
 		//Convierto el nombre a palabra con la primera en mayusculas
 		nombre = toUpperPrimeraLetra(nombre);
+		//Si es menor que 4, uso sin especificar
 		if(nombre.length()<4)
 			nombre="Sin especificar";
 		
 		this.nombre = nombre;
 	}
 	public void setApellidos(String apellidos) {
-		//Convierto el nombre a palabra con la primera en mayusculas
-		nombre = toUpperPrimeraLetra(nombre);
-		if(nombre.length()<4)
-			nombre="Sin especificar";
+		apellidos = convertirNombresPropios(apellidos);
 		
-		this.nombre = nombre;
+		//Si es menor que 4, uso sin especificar
+		if(apellidos.length() < 4 )
+			apellidos="Sin especificar";
+		
+		this.apellidos = apellidos;
 	}
-	
 	public void setAnio(int anio) {
 		if(anio<1900 || anio>2020)
 			anio = 2000;
 		
 		this.anioNacimiento = anio;
 	}
-	
 	public void setMes(int mes) {
 		if(mes<1 || mes>12)
 			mes = 1;
 		
 		this.mesNacimiento = mes;
 	}
-	
 	public void setDia(int dia) {
 		if(dia<1 || dia>31)
 			dia = 1;
 		
 		this.diaNacimiento = dia;
 	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
 	
+	//Métodos y funciones
+	public String mostrarEdad() {
+		return "En la actualidad tengo "+getEdad()+" años";
+	}
+	public String saludar() {
+		return "Hola soy "+this.nombre+" "+this.apellidos+" y naci el "+this.diaNacimiento+"/"+this.mesNacimiento+"/"+this.anioNacimiento;
+	}
 	private String toUpperPrimeraLetra(String palabra) {
 		String npropio = palabra;
-		//TODO implementar funcion
-		
+		String letra=palabra.substring(0,1).toUpperCase();
+		npropio=letra+npropio.substring(1);
+	
 		return npropio;
+	}
+	private String convertirNombresPropios(String nombrespropios) {
+		//Convierto los apellidos en un array de strings separandolo por espacios
+		String[] array_propios = nombrespropios.split(" ");
+
+		//Convierto cada elemento del array , en una palabra con la primera en mayusculas
+		for (int i=0;i<array_propios.length;i++)
+			array_propios[i] = toUpperPrimeraLetra(array_propios[i]);
+
+		//Creo la cadena de apellidos ya formateada concatenando los apellidos del array
+		String nombres_formateado = "";
+		for (int i=0;i<array_propios.length;i++)
+			nombres_formateado = nombres_formateado + array_propios[i] +" ";
+
+		return nombres_formateado;
 	}
 }
