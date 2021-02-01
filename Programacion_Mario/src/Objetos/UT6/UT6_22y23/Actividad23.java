42b2c29254718beeee9f102e1ce6274461c22007
MarioAguirre <MarioAguirre@DESKTOP-RCDC17H>
2021-02-01 23:24:55 +0100

    01/02/2021 - CASA

--- Programacion_Mario/src/Objetos/UT6/UT6_22y23/Actividad23.java
+++ Programacion_Mario/src/Objetos/UT6/UT6_22y23/Actividad23.java
@@ -17,8 +17,9 @@ public class Actividad23 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner ScannerMenu = new Scanner(System.in);
 		Scanner ScannerInt = new Scanner(System.in);
-		Scanner Scanner = new Scanner(System.in);
+		Scanner ScannerString = new Scanner(System.in);
 		
 		String titulo;
 		String autor;
@@ -29,40 +30,95 @@ public class Actividad23 {
 		ArrayList<Libro> libros=new ArrayList<Libro>();
 		
 		do {
-			System.out.println("\nPulsa:\n\t1: Alta Libro.\n\t2: Consultar Libro.\n\t3: Baja Libro.\n\t4: Prestamo Libro.\n\t0: Salir.");
-			opcion = ScannerInt.nextInt();
+			opcion = opciones(ScannerMenu);
 			
+			if(opcion==0) {
+				System.out.println("Has escogido la opcion 0");
+				System.out.println("Gracias.");
+			}
 			if(opcion==1) {
 				System.out.println("Has escogido la opcion 1");
 				System.out.println("Titulo de tu libro: ");
-				titulo = Scanner.nextLine();
+				titulo = ScannerString.nextLine();
 				System.out.println("Autor de tu libro: ");
-				autor = Scanner.nextLine();
+				autor = ScannerString.nextLine();
 				System.out.println("Nº de Libros: ");
 				ejemplares = ScannerInt.nextInt();
 				System.out.println("Nº de Libros prestados: ");
 				prestados = ScannerInt.nextInt();
 				libros.add(new Libro(titulo, autor, ejemplares, prestados));
-				System.out.println("Libro dado de alta. Nº: ");
+				System.out.println("Libro dado de alta.");
 	
 			}
 			if(opcion==2) {
 				System.out.println("Has escogido la opcion 2");
 				System.out.println("Introduce el titulo del libro: ");
-				titulo = Scanner.nextLine();
+				titulo = ScannerString.nextLine();
 				for(int i=0; i<libros.size(); i++) {
 					if(libros.get(i).getTitulo().equals(titulo)) {
-						System.out.println("Titulo: " + libros.get(i).getTitulo() + "\nAutor: " + libros.get(i).getAutor() + "\nEjemplares: " + libros.get(i).getEjemplares() + "\nPrestados: " + libros.get(i).getPrestados());
+						System.out.println("Titulo: "
+								+ libros.get(i).getTitulo()
+								+ "\nAutor: "
+								+ libros.get(i).getAutor()
+								+ "\nEjemplares: "
+								+ libros.get(i).getEjemplares()
+								+ "\nPrestados: "
+								+ libros.get(i).getPrestados());
+					}else {
+						System.out.println("Libro no encontrado.");
 					}
 				}
 			}
 			if(opcion==3) {
 				System.out.println("Has escogido la opcion 3");
-				//REMOVE
+				System.out.println("Introduce el titulo del libro que quiere borrar: ");
+				titulo = ScannerString.nextLine();
+				for(int i=0; i<libros.size(); i++) {
+					if(libros.get(i).getTitulo().equals(titulo)) {
+						libros.remove(i).getTitulo().equals(titulo);
+						System.out.println("Borrado.");
+					}else {
+						System.out.println("Libro no encontrado.");
+					}
+				}
 			}
+			if(opcion==4) {
+				System.out.println("Has escogido la opcion 4");
+				System.out.println("Introduce el titulo del libro que quiere hacer un prestamo: ");
+				titulo = ScannerString.nextLine();
+//				for(int i=0; i<libros.size(); i++) {
+//					if(libros.get(i).getTitulo().equals(titulo)) {
+//						libros.get(i).setPrestados(prestados);
+//						System.out.println("Libro prestado.");
+//					}else {
+//						System.out.println("Libro no encontrado.");
+//					}
+//				}
+			}
+			if(opcion==5) {
+				System.out.println("Has escogido la opcion 5");
+				System.out.println("Introduce el titulo del libro que quieres devolver: ");
+				titulo = ScannerString.nextLine();
+//				for(int i=0; i<libros.size(); i++) {
+//					if(libros.get(i).getTitulo().equals(titulo)) {
+//						libros.get(i).setPrestados(prestados);
+//						System.out.println("Libro devuelto.");
+//					}else {
+//						System.out.println("Libro no encontrado.");
+//					}
+//				}
+			}
+			System.out.println("");
 		}while(opcion!=0);
 		
 		ScannerInt.close();
-		Scanner.close();
+		ScannerString.close();
+	}
+	
+	private static int opciones(Scanner ScannerInt) {
+		int opcion = 0;
+		System.out.println("Pulsa:\n\t1: Alta Libro.\n\t2: Consultar Libro.\n\t3: Baja Libro.\n\t4: Prestamo Libro.\n\t0: Salir.");
+		opcion = ScannerInt.nextInt();
+		return opcion;
 	}
 }
