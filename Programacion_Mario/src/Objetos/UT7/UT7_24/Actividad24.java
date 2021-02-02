442cf567e961f2fd9835e7f21450cf7ce4d02ad1
MarioAguirre <MarioAguirre@DESKTOP-KF8T2TO.localdomain>
2021-02-02 13:40:39 +0100

    02/02/2021 - EEP

--- Programacion_Mario/src/Objetos/UT7/UT7_24/Actividad24.java
+++ Programacion_Mario/src/Objetos/UT7/UT7_24/Actividad24.java
@@ -15,12 +15,20 @@ public class Actividad24 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		NIF minif = new NIF(18082739, 'R');
-		System.out.println(minif.getDNI() + "-" + minif.getLetra());
-		minif.setLetra('U');
-		System.out.println(minif.getDNI() + "-" + minif.getLetra());
-		minif.setLetra('R');
-		System.out.println(minif.getDNI() + "-" + minif.getLetra());
+		NIF mio = new NIF();
+		System.out.println(mio);
+		
+		mio.setDNI(18082739);
+		System.out.println(mio);
+
+		
+		
+//		NIF minif = new NIF(18082739, 'R');
+//		System.out.println(minif.getDNI() + "-" + minif.getLetra());
+//		minif.setLetra('U');
+//		System.out.println(minif.getDNI() + "-" + minif.getLetra());
+//		minif.setLetra('R');
+//		System.out.println(minif.getDNI() + "-" + minif.getLetra());
 	}
 
 }
--- Programacion_Mario/src/Objetos/UT7/UT7_24/NIF.java
+++ Programacion_Mario/src/Objetos/UT7/UT7_24/NIF.java
@@ -1,40 +1,35 @@
 package Objetos.UT7.UT7_24;
 
-import java.util.ArrayList;
-
-import Objetos.UT6.UT6_22y23.Libro;
-
 public class NIF {
 	private int DNI;
 	private char letra;
 	
 	public NIF() {
-		setDNI(DNI);
-		setLetra(letra);
+		DNI=0;
+		letra=' ';
 	}
 	
-	public NIF(int DNI, char letra) {
-		
+	public NIF(int DNI) {
+		this.DNI=DNI;
+		this.letra=calcularLetra();
 	}
-
 	public int getDNI() {
 		return DNI;
 	}
-
 	public void setDNI(int DNI) {
 		this.DNI = DNI;
+		this.letra=calcularLetra();
 	}
-
-	public char getLetra() {
-		return letra;
-	}
-
-	public void setLetra(char letra) {
-		this.letra = letra;
-	}
-	
 	private char calcularLetra() {
-        char[] letras = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        char[] letras = {'T','R','W','A','G','M','Y','F','P',
+        		'D','X','B','N','J','Z','S','Q','V','H','L',
+        		'C','K','E'};
         return letras[DNI % 23];
     }
+	@Override
+	public String toString() {
+		return String.format("%08d", DNI)+'-'+letra;
+	}
+
+	
 }
